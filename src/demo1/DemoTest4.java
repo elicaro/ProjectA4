d9b36b859753a540d63b7f61b5b60da9cd880c9b
carol <carol@10.0.0.241>
2025-01-29 21:05:58 -0500

    Practice method Git

--- src/demo1/DemoTest4.java
+++ src/demo1/DemoTest4.java
@@ -3,9 +3,23 @@ package demo1;
 public class DemoTest4 {
 
 	public static void main (String args[]) {
-		System.out.println("Java");
+		System.out.println("Practice Git");
+		
+		
+		int a = 38;
+		int b = 8;
+		
+		int result = a + b;
+		
+		System.out.println(result);
+		
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
 	
 	}
 	
 	
-}
+
